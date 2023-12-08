4dea22ac38cce3e4e40842ba1aa6e43af2114cb4
mrt <mrt.konsap@gmail.com>
2023-12-08 21:21:41 +0200

    main refactor and pom update

--- src/main/kotlin/aoc/Main.kt
+++ src/main/kotlin/aoc/Main.kt
@@ -2,26 +2,27 @@ package aoc
 
 
 fun main(args: Array<String>) {
-    if (args.size > 1 && args[0] == "run") {
-        val target = args[1].split("-").map { it.toInt() }
-
-        val year = target[0]
-        val day = target[1]
-
-        println("https://adventofcode.com/$year/day/$day")
-
-        val puzzles: List<Puzzle> = if (target.size == 3) {
-            listOf(findPuzzle(year, day, target[2]) ?: throw IllegalArgumentException(args[1]))
-        } else {
-            findPuzzles(year, day)
-        }
-
-        if (args.size > 2) {
-            puzzles.forEach { it.run(args[2]) }
-        } else {
-            puzzles.forEach { it.run() }
-        }
+    val input = if (args.size > 1 && args[0] == "run") {
+        args[1]
+    } else {
+        print("year-day-part?>")
+        readln()
     }
+
+    val target = input.split("-").map { it.toInt() }
+    val year = target[0]
+    val day = target[1]
+    val part = if (target.size == 3) target[2] else null
+
+    println("https://adventofcode.com/$year/day/$day")
+
+    val puzzles: List<Puzzle> = if (part != null) {
+        listOf(findPuzzle(year, day, part) ?: throw IllegalArgumentException(input))
+    } else {
+        findPuzzles(year, day)
+    }
+
+    puzzles.forEach { it.run() }
 }
 
 
